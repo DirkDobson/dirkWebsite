efe22031654fa440877bb3b1a35e6963fbedc155
Dirk Dobson <dirkvancouver@live.com>
2018-11-21 17:18:15 -0700

    styled-components

--- src/Home.js
+++ src/Home.js
@@ -1,11 +1,19 @@
 import React from 'react'
+import styled from 'styled-components';
 
 class Home extends React.Component {
   render() {
     return (
-      <h1>Welcome to my website</h1>
+      <Header>Welcome To My Website</Header>
     )
   }
 }
+
+const Header = styled.div`
+  background-color: #00CED1;
+  width: 100%;
+  height 3em;
+  text-align: center;
+`
 
 export default Home
