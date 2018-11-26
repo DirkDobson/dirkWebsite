241e67858c9e8373a0f647bba2d6abdc4e8c86a3
Dirk Dobson <dirkvancouver@live.com>
2018-11-25 20:41:46 -0700

    variables

--- src/Game.js
+++ src/Game.js
@@ -6,6 +6,25 @@ class Game extends React.Children {
   }
   updateCanvas() {
     const ctx = this.refs.canvas.getContext('2d');
+    let x = this.canvas.width/2;
+    let y = this.canvas.hieght-30;
+    let dx = 5;
+    let dy = -5;
+    const ballRadius = 10;
+    const paddleHeight = 10;
+    const paddleWidth = 75;
+    let paddleX = (this.canvas.width-paddleWidth)/2;
+    let rightPressed = false;
+    let leftPressed = false;
+    let brickRowCount = 3;
+    let brickColumnCount = 5;
+    const brickWidth = 75;
+    const brickHeight = 20;
+    const brickOffsetTop = 30;
+    const brickOffsetLeft = 30;
+    let ballColor = 0;
+    let score = 0;
+    let lives = 3;
   }
   render() {
     return (
