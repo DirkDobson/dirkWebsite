89efaa89b2655122bfbbc55973802e3761e8ed42
Dirk Dobson <dirkvancouver@live.com>
2018-11-25 20:09:12 -0700

    start of canvas

--- src/Game.js
+++ src/Game.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
 class Game extends React.Children {
-
+  componentDidMount() {
+    this.updateCanvas();
+  }
+  updateCanvas() {
+    const ctx = this.refs.canvas.getContext('2d');
+  }
+  render() {
+    return (
+      <canvas ref="canvas" width={300} height={300}/>
+    )
+  }
 }
 
 export default Game
