b8067db6000404716f66e41060fe206c4dceefce
Dirk Dobson <dirkvancouver@live.com>
2019-03-18 22:50:16 -0600

    remove some rain

--- rain.js
+++ rain.js
@@ -10,8 +10,8 @@ var y2 = 0
 function drawRain() {
   can.clearRect(0, 0, canvas.width, canvas.height);
   rainDrop0();
-  rainDrop1();
-  rainDrop2();
+  // rainDrop1();
+  // rainDrop2();
   requestAnimationFrame(drawRain);
 }
 
@@ -29,38 +29,38 @@ function rainDrop0() {
   }
 }
 
-function rainDrop1() {
-  can.beginPath();
-  can.fillStyle = "lightblue";
-  can.arc(x1, y1, 5, 5.75, 3.66, false);
-  can.bezierCurveTo(x1, y1, x1, y1, x1 + 5, y1 - 5)
-  can.bezierCurveTo(x1 - 5, y1 - 5, x1, y1, x1 -5, y1 - 5)
-  can.fill();
-  can.closePath();
-  y1 = y1 + 6;
-  if ( y1 === canvas.width) {
-    y1 = 0
-    x1 = canvas.width/2 + Math.floor(Math.random() * 100)
-  }
-}
+// function rainDrop1() {
+//   can.beginPath();
+//   can.fillStyle = "lightblue";
+//   can.arc(x1, y1, 5, 5.75, 3.66, false);
+//   can.bezierCurveTo(x1, y1, x1, y1, x1 + 5, y1 - 5)
+//   can.bezierCurveTo(x1 - 5, y1 - 5, x1, y1, x1 -5, y1 - 5)
+//   can.fill();
+//   can.closePath();
+//   y1 = y1 + 6;
+//   if ( y1 === canvas.width) {
+//     y1 = 0
+//     x1 = canvas.width/2 + Math.floor(Math.random() * 100)
+//   }
+// }
 
-function rainDrop2() {
-  can.beginPath();
-  can.fillStyle = "lightblue";
-  can.arc(x2, y2, 5, 5.75, 3.66, false);
-  can.bezierCurveTo(x2, y2, x2, y2, x2 + 5, y2 - 5)
-  can.bezierCurveTo(x2 - 5, y2, x2, y2, x2 -5, y2 - 5)
-  can.fill();
-  can.closePath();
-  y2 += 6;
-  if ( y2 === canvas.width) {
-    y2 = 0
-    x2 = canvas.width/3 + Math.floor(Math.random() * 100)
-  }
-}
+// function rainDrop2() {
+//   can.beginPath();
+//   can.fillStyle = "lightblue";
+//   can.arc(x2, y2, 5, 5.75, 3.66, false);
+//   can.bezierCurveTo(x2, y2, x2, y2, x2 + 5, y2 - 5)
+//   can.bezierCurveTo(x2 - 5, y2, x2, y2, x2 -5, y2 - 5)
+//   can.fill();
+//   can.closePath();
+//   y2 += 6;
+//   if ( y2 === canvas.width) {
+//     y2 = 0
+//     x2 = canvas.width/3 + Math.floor(Math.random() * 100)
+//   }
+// }
 
-function rainHit(x,y) {
+// function rainHit(x,y) {
   
-}
+// }
 
 drawRain();
